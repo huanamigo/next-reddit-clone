e4e70009ab2396523587ecdc0021ef6be96911a4
Artur Furtak <arturfurtak5@gmail.com>
2024-07-11 21:47:38 +0200

    added create new post handler

--- src/actions/create-post.ts
+++ src/actions/create-post.ts
@@ -1,5 +1,90 @@
 'use server';
 
-export async function createPost() {
-  console.log('createPost');
+import { auth } from '@/auth';
+import { db } from '@/db';
+import { Post } from '@prisma/client';
+import paths from '@/paths';
+import { revalidatePath } from 'next/cache';
+import { z } from 'zod';
+import { redirect } from 'next/navigation';
+
+interface CreatePostFormState {
+  errors: {
+    title?: string[];
+    content?: string[];
+    _form?: string[];
+  };
+}
+
+const createPostSchema = z.object({
+  title: z.string().min(3, { message: 'Must be longer than 3 characters' }),
+  content: z.string().min(10, { message: 'Must be longer than 10 characters' }),
+});
+
+export async function createPost(
+  slug: string,
+  formState: CreatePostFormState,
+  formData: FormData
+): Promise<CreatePostFormState> {
+  const result = createPostSchema.safeParse({
+    title: formData.get('title'),
+    content: formData.get('content'),
+  });
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  const session = await auth();
+
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ['You have to be signed in to add a Post'],
+      },
+    };
+  }
+
+  const topic = await db.topic.findFirst({
+    where: { slug },
+  });
+
+  if (!topic) {
+    return {
+      errors: {
+        _form: ['Cannot find that Topic'],
+      },
+    };
+  }
+
+  let post: Post;
+  try {
+    post = await db.post.create({
+      data: {
+        title: result.data.title,
+        content: result.data.content,
+        userId: session.user.id,
+        topicId: topic.id,
+      },
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return {
+        errors: {
+          _form: [error.message],
+        },
+      };
+    } else {
+      return {
+        errors: {
+          _form: ['Something went wrong'],
+        },
+      };
+    }
+  }
+
+  revalidatePath(paths.showPost(slug, post.id));
+  redirect(paths.showPost(slug, post.id));
 }
--- src/components/posts/post-create-form.tsx
+++ src/components/posts/post-create-form.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { createPost } from '@/actions';
 import {
   Popover,
@@ -7,30 +9,49 @@ import {
   Textarea,
   Input,
 } from '@nextui-org/react';
+import { useFormState } from 'react-dom';
+import FormButton from '../common/form-button';
 
-export default function PostCreateForm() {
+interface PostCreateFormProps {
+  slug: string;
+}
+
+export default function PostCreateForm({ slug }: PostCreateFormProps) {
+  const [formState, action] = useFormState(createPost.bind(null, slug), {
+    errors: {},
+  });
+
   return (
     <Popover>
       <PopoverTrigger>
         <Button color="primary">Create a post</Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form action={createPost}>
+        <form action={action}>
           <div className="flex flex-col gap-4 p-4 w-80">
             <h3 className="text-lg">Create a post</h3>
             <Input
               name="title"
               label="Title"
-              labelPlacement="outside"
               placeholder="Title"
+              labelPlacement="outside"
+              isInvalid={!!formState.errors.title}
+              errorMessage={formState.errors.title?.join(', ')}
             />
             <Textarea
               name="content"
               label="Content"
+              placeholder="Content"
               labelPlacement="outside"
-              placeholder="Name"
+              isInvalid={!!formState.errors.content}
+              errorMessage={formState.errors.content?.join(', ')}
             />
-            <Button type="submit">Submit</Button>
+            {formState.errors._form ? (
+              <div className="rounded p-2 bg-red-200 border border-red-400">
+                {formState.errors._form.join(', ')}
+              </div>
+            ) : null}
+            <FormButton>Submit</FormButton>
           </div>
         </form>
       </PopoverContent>
