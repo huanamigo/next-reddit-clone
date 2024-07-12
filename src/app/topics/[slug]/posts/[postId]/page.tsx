78742c1d27f3570dc87154273bb4face7457892c
Artur Furtak <arturfurtak5@gmail.com>
2024-07-12 18:51:03 +0200

    handled post showing

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,7 +1,30 @@
-export default function PostShowPage() {
+import Link from 'next/link';
+import PostShow from '@/components/posts/post-show';
+import CommentList from '@/components/comments/comment-list';
+import CommentCreateForm from '@/components/comments/comment-create-form';
+import paths from '@/paths';
+
+interface PostShowPageProps {
+  params: {
+    slug: string;
+    postId: string;
+  };
+}
+
+export default async function PostShowPage({ params }: PostShowPageProps) {
+  const { slug, postId } = params;
+
   return (
-    <div>
-      <p>Show post page</p>
+    <div className="space-y-3">
+      <Link
+        className="underline decoration-solid"
+        href={paths.showPost(slug, postId)}
+      >
+        {'< '}Back to {slug}
+      </Link>
+      {/* <PostShow /> */}
+      {/* <CommentCreateForm postId={postId} startOpen /> */}
+      {/* <CommentList comments={comments} /> */}
     </div>
   );
 }
