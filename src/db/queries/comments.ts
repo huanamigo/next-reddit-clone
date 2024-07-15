513e14aa1d406ef41f9a8c9aa174d7d08ffa71b3
Artur Furtak <arturfurtak5@gmail.com>
2024-07-15 20:30:25 +0200

    fixed comments showing and replying

---
+++ src/db/queries/comments.ts
@@ -0,0 +1,27 @@
+import { Comment } from '@prisma/client';
+import { db } from '..';
+
+export type CommentWithAuthor = Comment & {
+  user: {
+    name: string | null;
+    image: string | null;
+  };
+};
+
+export function fetchCommentByPostId(
+  postId: string
+): Promise<CommentWithAuthor[]> {
+  return db.comment.findMany({
+    where: {
+      postId: postId,
+    },
+    include: {
+      user: {
+        select: {
+          name: true,
+          image: true,
+        },
+      },
+    },
+  });
+}
